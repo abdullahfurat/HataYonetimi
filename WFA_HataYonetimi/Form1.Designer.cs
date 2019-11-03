7211e5982f4344c3aceb50874c83dac2af8c45cf
muratvuranok <muratvuranok@hotmail.com>
2019-11-03 20:42:00 +0300

    Add project files.

---
+++ WFA_HataYonetimi/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WFA_HataYonetimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOrnek1 = new System.Windows.Forms.Button();
+            this.btnOrnek2 = new System.Windows.Forms.Button();
+            this.btnOrnek3 = new System.Windows.Forms.Button();
+            this.btnOrnek4 = new System.Windows.Forms.Button();
+            this.btnOrnek5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(443, 53);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnOrnek1
+            // 
+            this.btnOrnek1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek1.Location = new System.Drawing.Point(12, 74);
+            this.btnOrnek1.Name = "btnOrnek1";
+            this.btnOrnek1.Size = new System.Drawing.Size(443, 53);
+            this.btnOrnek1.TabIndex = 1;
+            this.btnOrnek1.Text = "Hata Yönetimsiz";
+            this.btnOrnek1.UseVisualStyleBackColor = true;
+            this.btnOrnek1.Click += new System.EventHandler(this.btnOrnek1_Click);
+            // 
+            // btnOrnek2
+            // 
+            this.btnOrnek2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek2.Location = new System.Drawing.Point(12, 133);
+            this.btnOrnek2.Name = "btnOrnek2";
+            this.btnOrnek2.Size = new System.Drawing.Size(443, 53);
+            this.btnOrnek2.TabIndex = 1;
+            this.btnOrnek2.Text = "Hata Yönetimli";
+            this.btnOrnek2.UseVisualStyleBackColor = true;
+            this.btnOrnek2.Click += new System.EventHandler(this.btnOrnek2_Click);
+            // 
+            // btnOrnek3
+            // 
+            this.btnOrnek3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek3.Location = new System.Drawing.Point(12, 192);
+            this.btnOrnek3.Name = "btnOrnek3";
+            this.btnOrnek3.Size = new System.Drawing.Size(443, 53);
+            this.btnOrnek3.TabIndex = 1;
+            this.btnOrnek3.Text = "Hata Yönetimli - Mesaj";
+            this.btnOrnek3.UseVisualStyleBackColor = true;
+            this.btnOrnek3.Click += new System.EventHandler(this.btnOrnek3_Click);
+            // 
+            // btnOrnek4
+            // 
+            this.btnOrnek4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek4.Location = new System.Drawing.Point(12, 251);
+            this.btnOrnek4.Name = "btnOrnek4";
+            this.btnOrnek4.Size = new System.Drawing.Size(443, 53);
+            this.btnOrnek4.TabIndex = 1;
+            this.btnOrnek4.Text = "Hata Yönetimli - Mesaj";
+            this.btnOrnek4.UseVisualStyleBackColor = true;
+            this.btnOrnek4.Click += new System.EventHandler(this.btnOrnek4_Click);
+            // 
+            // btnOrnek5
+            // 
+            this.btnOrnek5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrnek5.Location = new System.Drawing.Point(12, 310);
+            this.btnOrnek5.Name = "btnOrnek5";
+            this.btnOrnek5.Size = new System.Drawing.Size(443, 53);
+            this.btnOrnek5.TabIndex = 1;
+            this.btnOrnek5.Text = "Hata Yönetimli - Finally";
+            this.btnOrnek5.UseVisualStyleBackColor = true;
+            this.btnOrnek5.Click += new System.EventHandler(this.btnOrnek5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 371);
+            this.Controls.Add(this.btnOrnek5);
+            this.Controls.Add(this.btnOrnek4);
+            this.Controls.Add(this.btnOrnek3);
+            this.Controls.Add(this.btnOrnek2);
+            this.Controls.Add(this.btnOrnek1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOrnek1;
+        private System.Windows.Forms.Button btnOrnek2;
+        private System.Windows.Forms.Button btnOrnek3;
+        private System.Windows.Forms.Button btnOrnek4;
+        private System.Windows.Forms.Button btnOrnek5;
+    }
+}
+
---
+++ WFA_HataYonetimi/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WFA_HataYonetimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrnek1_Click(object sender, EventArgs e)
+        {
+            int transactionNo = Convert.ToInt32(textBox1.Text);
+            MessageBox.Show("Kayıt işlemi başarılı");
+        }
+
+        private void btnOrnek2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int transactionNo = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Kayıt işlemi başarılı");
+            }
+            catch
+            {
+                MessageBox.Show("İşlem Hatası");
+            }
+        }
+
+        private void btnOrnek3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int transactionNo = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Kayıt işlemi başarılı");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("İşlem Hatası");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOrnek4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int transactionNo = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Kayıt işlemi başarılı");
+            }
+            catch (DivideByZeroException ex)
+            {
+                // A birimi kontrol edecek.
+                // LogManager("A",ex);
+            }
+            catch (FormatException ex)
+            {
+                // B birimi kontrol edecek
+                // LogManager("B",ex);
+            }
+            catch (SqlException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("İşlem Hatası");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOrnek5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int transactionNo = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Kayıt işlemi başarılı");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("İşlem Hatası");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Her işlem sonrası mutlaka çalışırım :)");
+                // Con.close() 
+            }
+        }
+    }
+}
